ae1167f0e9e80712b6b21ac95060d60f413978a3
none <none>
2016-12-12 19:58:57 +0200

    number4

---
+++ student.py
@@ -0,0 +1,11 @@
+class Student :
+    def __init__(self,name,hometown,age,height,favorite_ice_cream_flaver)
+    self.name=name
+    self.hometown=hometown
+    self.age=age
+    self.height=height
+    self.favorite_ice_cream_flaver=favorite_ice_cream_flaver
+
+    def print_summery(self):
+        print("hi, my name is" + self.name)
+        
